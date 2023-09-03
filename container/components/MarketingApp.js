528674ee0d1afb3cdf887be0a56078d25751a7aa
Gaurav Mithas <gaurav.mithas@publicissapient.com>
2023-09-03 20:57:52 +0530

    debug pipeline

--- container/components/MarketingApp.js
+++ container/components/MarketingApp.js
@@ -10,7 +10,7 @@ export default function MarketingApp() {
     }, [])
 
     return (
-        <div ref={marketingRef}>Marketing App</div>
+        <div ref={marketingRef}>MarketingApp</div>
 
     )
 }
