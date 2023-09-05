c44f5972d0e7c4cbf0e82cef0b4db432b10f9e57
Gaurav Mithas <gaurav.mithas@publicissapient.com>
2023-09-05 09:13:30 +0530

    setup aws cli

--- container/src/App.js
+++ container/src/App.js
@@ -1,14 +1,47 @@
-import React, { useRef } from "react";
-import MarketingApp from "../components/MarketingApp";
-import Header from "../components/Header";
+import React, { useRef, useState, lazy, useEffect, Suspense } from "react";
+import { Router, Route, Switch, Redirect } from 'react-router-dom';
 
+import Header from "../components/Header";
+import { createBrowserHistory } from 'history';
+import { StylesProvider, createGenerateClassName } from "@material-ui/core/styles";
+const MarketingLazy = lazy(() => import('../components/MarketingApp'));
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co',
+});
+
+const history = createBrowserHistory();
 function App(props) {
+    const [isSignedIn, setIsSignedIn] = useState(false);
+
+    useEffect(() => {
+        if (isSignedIn) {
+            history.push('/dashboard');
+        }
+    }, [isSignedIn]);
     return (
-        <>
-            <Header />
-            <hr />
-            < MarketingApp />
-        </>
+        <Router history={history}>
+            <StylesProvider generateClassName={generateClassName}>
+                <div>
+                    <Header
+                        onSignOut={() => setIsSignedIn(false)}
+                        isSignedIn={isSignedIn}
+                    />
+                    <Suspense fallback={<span>loading...</span>}>
+                        <Switch>
+                            {/*   <Route path="/auth">
+                                <AuthLazy onSignIn={() => setIsSignedIn(true)} />
+                            </Route>
+                            <Route path="/dashboard">
+                                {!isSignedIn && <Redirect to="/" />}
+                                <DashboardLazy />
+                            </Route> */}
+                            <Route path="/" component={MarketingLazy} />
+                        </Switch>
+                    </Suspense>
+                </div>
+            </StylesProvider>
+        </Router>
+
     )
 }
 export default App
